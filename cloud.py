434d156211a8c2df68a6341f8d53fbc566c2c1e1
mansichotaliya <mansi.chotaliya.comp.2023@vpkbiet.org>
2025-12-29 07:17:10 -0800

    New Commit

--- cloud.py
+++ cloud.py
@@ -1,2 +1,4 @@
 print("To demonstrate Software as a service using a cloud service provider.")
 print("We use Git Hub for Saas.")
+print("Cloud Computing Assignment No. 3")
+print("Software as a Service")
